06b8261caad6781bd8ae15955e073e75bcf751d9
taruc <bro_brada123@hotmail.com>
2018-12-19 17:37:56 +0800

    initial commit

---
+++ app/src/main/java/my/edu/taruc/lab42database/User.java
@@ -0,0 +1,56 @@
+package my.edu.taruc.lab42database;
+
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+@Entity(tableName = "user")
+public class User {
+    @PrimaryKey
+@NonNull
+    private String phone;
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @ColumnInfo(name = "first_name")
+    private String firstName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    private String lastName;
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public User(){
+
+    }
+
+    public User(String phone, String firstName, String lastName) {
+        this.phone = phone;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+
+
+}
